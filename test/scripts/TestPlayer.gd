47438fc3914b5a8d31c700cc9fd0d24c1febc45d
unknown <miurary@oregonstate.edu>
2022-07-10 15:26:01 -0700

    Added attack animations to player AnimationPlayer | Added attack node to player AnimationTree | Reformatted TestPlayer.gd script to allow for new actions to be added using state | Changed movement values in TestPlayer.gd so player doesn't slide as much | Added code to run attack animations for player

--- test/scripts/TestPlayer.gd
+++ test/scripts/TestPlayer.gd
@@ -1,16 +1,34 @@
 extends KinematicBody2D
 
-const ACCELERATION = 500
-const MAX_SPEED = 400
-const FRICTION = 1000
+enum {
+	MOVE,
+	ATTACK
+}
 
-var velocity = Vector2.ZERO
+const ACCELERATION = 500
+const MAX_SPEED = 200
+const FRICTION = 1000
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+var state = MOVE
+var velocity = Vector2.ZERO
+
+func _ready():
+	animationTree.active = true
+	print("Player ready function finished")
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			moveState(delta)
+		ATTACK:
+			attackState()
+	
+	
+func moveState(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -19,12 +37,25 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	#print(velocity)
+	move()
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+	
+func attackState():
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+	
+func attackAnimationFinished():
+	state = MOVE
+
+func move():
 	velocity = move_and_slide(velocity)
 #end func _physics_process
